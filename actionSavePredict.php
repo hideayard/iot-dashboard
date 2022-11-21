f53d3ce6e329c3a9e6bb30ccc8b52add83e53ff0
hideayard <hideayard@gmail.com>
2022-11-21 15:52:14 +0700

    update action savepredict

--- actionSavePredict.php
+++ actionSavePredict.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once ('config/MysqliDb.php');
 include_once ("config/db.php");
 $db = new MysqliDb ('localhost', $dbuser, $dbpass, $dbname);
@@ -18,21 +20,21 @@ $tgl = (new \DateTime())->format('Y-m-d H:i:s');
   try
   {
 
-$label = ["s1","s2","s3","s4","s5","s6","s7","s8"];
+$label = ["s1","s2","s3","s4","s5","s6","s7","s8","s9"];
 $i=-1;
 if($id!="")
 {
     $data = Array ();
     $data += array('data_id' => $id);
-    $data += array('s'.($i+2) => floatval($predict[++$i]) );
-    $data += array('s'.($i+2) => floatval($predict[++$i]) );
-    $data += array('s'.($i+2) => floatval($predict[++$i]) );
-    $data += array('s'.($i+2) => floatval($predict[++$i]) );
-    $data += array('s'.($i+2) => floatval($predict[++$i]) );
-    $data += array('s'.($i+2) => floatval($predict[++$i]) );
-    $data += array('s'.($i+2) => floatval($predict[++$i]) );
-    $data += array('s'.($i+2) => floatval($predict[++$i]) );
-    $data += array('s'.($i+2) => floatval($predict[++$i]) );
+    $data += array('s1' => floatval($predict[0]) );
+    $data += array('s2' => floatval($predict[1]) );
+    $data += array('s3' => floatval($predict[2]) );
+    $data += array('s4' => floatval($predict[3]) );
+    $data += array('s5' => floatval($predict[4]) );
+    $data += array('s6' => floatval($predict[5]) );
+    $data += array('s7' => floatval($predict[6]) );
+    $data += array('s8' => floatval($predict[7]) );
+    $data += array('s9' => floatval($predict[8]) );
 
     $data += array('remark' => $label[$i]);
     $data += array('created_by' => 0);
